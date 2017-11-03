3074a3ce87a1e5997b01c678aa58bad8c4fa3427
Robert Laws <roblaws@me.com>
2017-11-03 15:40:09 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -36,6 +36,7 @@ class FiguresController < ApplicationController
   get '/figures/:id/edit' do
     @figure = Figure.find("#{params[:id]}")
     @titles = Title.all
+    @landmarks = Landmark.all
     erb :'figures/edit'
   end
 end
