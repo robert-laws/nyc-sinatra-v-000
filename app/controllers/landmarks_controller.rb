dd0fa471ae7244b4bfeb9a3491316f86422a8afc
Robert Laws <roblaws@me.com>
2017-11-03 16:24:12 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -10,7 +10,7 @@ class LandmarksController < ApplicationController
 
   get '/landmarks' do
     @landmarks = Landmark.all
-    erb :'landmarks/show'
+    erb :'landmarks/index'
   end
 
   get '/landmarks/:id' do
