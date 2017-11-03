3074929f6493865a65e36ba2b555afad1623b100
Robert Laws <roblaws@me.com>
2017-11-03 15:12:33 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -7,7 +7,7 @@ class FiguresController < ApplicationController
   end
 
   post '/figures/new' do
-    @figure = Figure.create(params[:figure])
+    @figure = Figure.create(name: params[figure][name])
 
     # redirect to "/figures/#{@figure.id}"
   end
