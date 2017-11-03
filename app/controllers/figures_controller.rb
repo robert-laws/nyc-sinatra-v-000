41beddb37de9ae8497a58506b0481df83aee2494
Robert Laws <roblaws@me.com>
2017-11-03 16:05:43 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -40,7 +40,7 @@ class FiguresController < ApplicationController
     erb :'figures/edit'
   end
 
-  post '/figures/:id' do
+  post '/figures/:id/edit' do
     @figure = Figure.find("#{params[:id]}")
     @figure.update(params[:figure])
 
